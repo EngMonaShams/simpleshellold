2178313a20e68d18422d0042e3e3cac668eca84d
EngMonaShams <133608386+EngMonaShams@users.noreply.github.com>
2023-09-23 00:48:42 +0300

    Update exit_func.c

--- exit_func.c
+++ exit_func.c
@@ -1,13 +1,5 @@
 #include "shell.h"
 
-/**
- * exit_shell - afunction that exit shell safely
- */
-void exit_shell(void)
-{
-	exit(0);
-}
-
 **
  * exit_alloc_error - afunction that exit shell safely
  */
